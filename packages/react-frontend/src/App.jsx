0b57db7b2672d63ec7c51fa917f1e2ca36cb7122
Juan Gonzalez <lujujose@gmail.com>
2025-10-27 11:05:01 -0700

    installed prettier

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import Chat from "./Chat";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className = "container">
-      <Chat onSend={(msg) => {
-        console.log(msg);
-      }} />
+    <div className="container">
+      <Chat
+        onSend={(msg) => {
+          console.log(msg);
+        }}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
