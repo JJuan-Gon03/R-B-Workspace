db3624270bc4133878aca001d9072b1b29f7d4ed
Atkin Rong <tyratus1318@gmail.com>
2025-11-11 14:34:46 -0800

    display gemini response

--- packages/react-frontend/src/Chat.jsx
+++ packages/react-frontend/src/Chat.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 function Chat({}) {
   const[text,setText]=useState("");
+  const[response,setResponse]=useState("")
+
   async function onSubmit(event){
     console.log("chat.js onSubmit")
     event.preventDefault()
     const res=await fetch("http://localhost:8000/response/"+encodeURIComponent(text))
     const res_text=await res.text();
-    console.log("gemini response: ", res_text)
+    setResponse(res_text);
   }
+  
   return(
     <div>
       <form onSubmit={onSubmit}>
@@ -17,6 +20,7 @@ function Chat({}) {
           onChange={(e)=>setText(e.target.value)}/>
         <button type="submit">Generate Outfit</button>
       </form>
+      <p>{response}</p>
     </div>
   );
 }
