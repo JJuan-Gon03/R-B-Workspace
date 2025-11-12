414252e6c2177f80678be4c399d040ddee9b9497
Atkin Rong <tyratus1318@gmail.com>
2025-11-11 16:54:14 -0800

    upload images to cloud

--- packages/react-frontend/src/Upload.jsx
+++ packages/react-frontend/src/Upload.jsx
@@ -3,12 +3,23 @@ import React, { useState } from "react";
 function Upload({}) {
   const[image,setImage]=useState(null);
 
-  const example_url="https://media.istockphoto.com/id/495204892/photo/sneakers.jpg?s=612x612&w=0&k=20&c=QSkl09_Rx2lvayG93dWBmoCsVPThoAB1VgcSyh6Jy_4="
-
   async function onSubmit(event){
     console.log("Upload.js onSubmit")
     event.preventDefault()
-    const res=await fetch("http://localhost:8000/gemini/parse_cloth/"+encodeURIComponent(example_url))
+
+    const formData=new FormData()
+    formData.append("file", image)
+    formData.append("upload_preset", "uploads")
+
+    const upload=await fetch(`https://api.cloudinary.com/v1_1/cloud_name/image/upload`, {
+      method:"POST",
+      body:formData,
+    });
+
+    const data=await upload.json()
+    console.log("url: ", data.secure_url)
+
+    const res=await fetch("http://localhost:8000/gemini/parse_cloth/"+encodeURIComponent(data.secure_url))
     const res_text=await res.text()
     console.log(res_text)
   }
