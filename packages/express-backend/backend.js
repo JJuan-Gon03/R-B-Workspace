08e793008d3d6805d5394c0defa99bc1c38dd4a6
Atkin Rong <tyratus1318@gmail.com>
2026-01-12 16:06:03 -0800

    database schema and basic functions

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import gemini from "./gemini.js";
 import wardrobe from "./wardrobe.js";
+import tags from "./tags.js"
 
 const app = express();
 const port = 8000;
@@ -34,4 +35,25 @@ app.get("/wardrobe/:user_id", async (req, res) => {
   const wd = await wardrobe.getWardrobe(req.params.user_id);
   res.status(201).send(wd);
 });
+
+app.delete("/wardrobe/:clothId", async (req, res) => {
+    await wardrobe.removeClothById(req.params.clothId);
+    res.status(200).send();
+});
+
+app.post("/tags", async (req, res) => {
+  tags.addTag(req.body);
+  res.status(201).send();
+});
+
+app.get("/tags/:user_id", async (req, res) => {
+  const ts = await tags.getTags(req.params.user_id);
+  res.status(200).send(ts);
+});
+
+app.delete("/tags/:tagId", async (req, res) => {
+    await tags.removeTagById(req.params.tagId);
+    res.status(200).send();
+});
+
 
