58c81ab55bdf737f79fce23ff44f700acfdea2c3
Atkin Rong <tyratus1318@gmail.com>
2026-02-05 12:57:34 -0800

    move test outsdie of src

---
+++ packages/react-frontend/tests/Upload2.test.jsx
@@ -0,0 +1,34 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import Upload2 from "../src/Upload2";
+import { vi } from "vitest";
+test("basic functionality", async () => {
+  vi.spyOn(global, "fetch").mockResolvedValueOnce({});
+  global.URL.createObjectURL = vi.fn(() => "blob:preview-url");
+
+  render(<Upload2 />);
+
+  fireEvent.click(screen.getByText("Upload Item"));
+  fireEvent.change(screen.getByPlaceholderText("Name"), {
+    target: { value: "example name" },
+  });
+
+  const select_color = screen.getByLabelText("color-select");
+  fireEvent.change(select_color, { target: { value: "Black" } });
+  expect(select_color).toHaveValue("Black");
+
+  const select_type = screen.getByLabelText("type-select");
+  fireEvent.change(select_type, { target: { value: "Shirts" } });
+  expect(select_type).toHaveValue("Shirts");
+
+  const fileInput = screen.getByLabelText("file-select");
+  const file = new File(["image"], "shirt.png", {
+    type: "image/png",
+  });
+  fireEvent.change(fileInput, {
+    target: { files: [file] },
+  });
+
+  const preview = screen.getByAltText("preview");
+  expect(preview).toBeInTheDocument();
+  expect(preview).toHaveAttribute("src", "blob:preview-url");
+});
