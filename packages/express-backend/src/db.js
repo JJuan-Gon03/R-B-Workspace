13576ca3a92f500f2ec3c0e89370e04218fbb1ad
Juan Gonzalez <lujujose@gmail.com>
2026-02-23 20:02:33 -0800

    Revert "connect to db and server"
    This reverts commit fc0330a2e1469d59ff681d69a6dc5c7ef7ee5fdd.

--- packages/express-backend/src/db.js
+++ packages/express-backend/src/db.js
@@ -2,12 +2,9 @@ import mongoose from "mongoose";
 
 mongoose.set("debug", true);
 
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/randb";
-
 async function connectDB() {
   mongoose
-    .connect(MONGODB_URI, {})
+    .connect("mongodb://localhost:27017/randb", {})
     .catch((error) => console.log("error connecting to mongodb:\n", error));
 }
 
