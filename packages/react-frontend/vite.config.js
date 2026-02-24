c09b28ce9dc01725ad36c8f817e1cc5509371929
Juan Gonzalez <lujujose@gmail.com>
2026-02-24 14:40:59 -0800

    format

--- packages/react-frontend/vite.config.js
+++ packages/react-frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "R-B-Workspace",
+  base: "/R-B-Workspace/",
   test: {
     environment: "jsdom",
     setupFiles: "./tests/setupTests.js",
