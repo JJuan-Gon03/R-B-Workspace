4bf97320228f7d0c9a1997a7b48d926eb843cdad
Atkin Rong <tyratus1318@gmail.com>
2026-02-27 17:43:21 -0800

    port set up in backend

--- packages/express-backend/src/server.js
+++ packages/express-backend/src/server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { app } from "./app.js";
 import { connectDB } from "./db.js";
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 
 connectDB()
   .then(() => {
